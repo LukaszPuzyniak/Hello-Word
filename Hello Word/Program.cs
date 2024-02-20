5c04d5eed3a1f7ca6e334b38becf801c79d9a3b4
Lukasz <Lukasz@DESKTOP-MGP8KSK>
2024-02-20 18:24:23 +0100

    Add project files.

---
+++ Hello Word/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lukasz"; 
+            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("My name is " + name);
+        }
+    }
+}
